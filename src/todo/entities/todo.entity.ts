a3732a10b8671f334776eb2791a5f5d56cb76eab
sinda <sindajeddey@gmail.com>
2020-12-25 20:35:17 +0100

    Implementing delete method

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -2,4 +2,5 @@ export class Todo {
   id: number;
   name: string;
   description: string;
+  constructor(name: string, description: string, id: number) {}
 }
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -6,10 +6,11 @@ import {
   NotFoundException,
   Param,
   Post,
-  Put, Query,
+  Put,
+  Query,
   Req,
-  Res
-} from "@nestjs/common";
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { Todo } from './entities/todo.entity';
 
@@ -22,7 +23,11 @@ export class TodoController {
   }
 
   @Get()
-  getTodos(@Req() request: Request, @Res() response: Response, @Query() queryParams): Todo[] {
+  getTodos(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Query() queryParams,
+  ): Todo[] {
     console.log(queryParams);
     response.send(this.todos); //overrides the return statement
     return this.todos;
@@ -49,9 +54,13 @@ export class TodoController {
     console.log("MAJ d'un to-do");
     return 'To Do mis à jour';
   }
-  @Delete()
-  deleteTodo(): string {
-    console.log("Suppression d'un to-do");
-    return 'To Do supprimé';
+  @Delete('/:id')
+  deleteTodo(@Param('id') id): string {
+    const index = this.todos.findIndex((todo) => todo.id === +id);
+    console.log(index);
+    if (index >= 0) {
+      this.todos.slice(index, index + 1);
+      return 'To do supprimé';
+    } else throw new NotFoundException(`Todo id ${id} not found`);
   }
 }
