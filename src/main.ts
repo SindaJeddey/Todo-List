bc2b25a4951e8f24c621820dece7743789ddb7d6
sinda <sindajeddey@gmail.com>
2020-12-26 12:56:24 +0100

    Pipes: transformation pipes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { HttpStatus, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.BAD_REQUEST}))
+  app.useGlobalPipes(new ValidationPipe({
+    errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+    transform: true,
+    whitelist: true, //Eliminate unnecessary data from request( extra that are not in the DTOs)
+    forbidNonWhitelisted: true
+    }))
   await app.listen(3000);
 }
 bootstrap();
