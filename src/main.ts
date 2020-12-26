e9eace074d941fad19ba4042fbff054f411dc351
sinda <sindajeddey@gmail.com>
2020-12-26 12:45:36 +0100

    Pipes: using validation pipe and the validations decorators (class-validator dependency)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { HttpStatus, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.BAD_REQUEST}))
   await app.listen(3000);
 }
 bootstrap();
--- src/todo/entities/Dtos/newTodo.dto.ts
+++ src/todo/entities/Dtos/newTodo.dto.ts
@@ -1,4 +1,18 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class NewTodoDto{
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6,{
+    message: "Name must have at least 6 characters"
+  })
+  @MaxLength(25,{
+    message: "Name must have at most 25 characters"
+  })
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
