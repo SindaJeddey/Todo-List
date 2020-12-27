433197d5b0e661b9ca52490ca1ac1668b31da057
sinda <sindajeddey@gmail.com>
2020-12-27 16:11:27 +0100

    Implementing soft and hard delete. Use of query builder

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { CvService } from "./cv.service";
 import { CvEntity } from "./entities/cv.entity";
 import { NewCvDto } from "./dto/new-cv.dto";
 import { UpdateCvDto } from "./dto/update-cv.dto";
+import { UpdateResult } from "typeorm";
 
 @Controller('cv')
 export class CvController {
@@ -13,6 +14,21 @@ export class CvController {
   @Get()
   async getAllCvs() : Promise<CvEntity[]>{
     return await this.cvService.getCvs();
+  }
+
+  @Get("/age/:minAge/:maxAge")
+  async getNbCvByAge(
+    @Param('minAge', ParseIntPipe) min,
+    @Param('maxAge', ParseIntPipe) max
+  ){
+    return await this.cvService.statCvNumberByAge(max,min);
+  }
+
+  @Get("/:id")
+  async getCv(
+    @Param('id',ParseIntPipe) id
+  ) : Promise<CvEntity>{
+    return await this.cvService.getCvById(id);
   }
 
   @Post()
@@ -30,4 +46,17 @@ export class CvController {
     return await this.cvService.updateCv(id,updates);
   }
 
+  @Delete('/:id')
+  async deleteCv(
+    @Param('id',ParseIntPipe) id
+  ) : Promise<CvEntity>{
+    return this.cvService.softDeleteCv(id);
+  }
+
+  @Get('/recover/:id')
+  async recoverCv(
+    @Param('id',ParseIntPipe) id
+  ): Promise<UpdateResult>{
+    return this.cvService.restoreCv(id);
+  }
 }
