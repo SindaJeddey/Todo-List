ca5f2e731e5b62b8ed7e76343ec51906b842f493
sinda <sindajeddey@gmail.com>
2020-12-27 18:33:12 +0100

    Implementing JWT login.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,20 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserEntity } from "./entities/user.entity";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 @Module({
-  imports:[TypeOrmModule.forFeature([UserEntity])],
+  imports:[
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule.register({
+      defaultStrategy:'jwt'
+    }),
+    JwtModule.register({
+      secret:process.env.SECRET
+    })],
   controllers: [UserController],
   providers: [UserService]
 })
