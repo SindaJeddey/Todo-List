de9b290bb477ff6e9c2f27506ad92f7a1020a2ee
sinda <sindajeddey@gmail.com>
2020-12-27 12:29:15 +0100

    Creating the repository for cv entity and injecting it in the cv service.

--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CvController } from './cv.controller';
 import { CvService } from './cv.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CvEntity } from "./entities/cv.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CvEntity])], // responsable de la creation d'un repository lié à cv entity
   controllers: [CvController],
   providers: [CvService]
 })
